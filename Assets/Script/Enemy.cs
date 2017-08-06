21acd276cfab7b5ca45e49204b44a77837e4177a
Etienne <fermivor@gmail.com>
2017-08-06 14:08:17 +0200

    début d'enemy

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	NavMeshAgent agent;
+	GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		target = GameObject.Find ("Player");
+		if (target == null)
+			Debug.Log ("No target found");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (target.transform.position);
+	}
+}
